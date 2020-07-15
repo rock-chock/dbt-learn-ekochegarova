0c95f0d60f42f739e03c4ab84b19df8a24ed8298
rock-chock <kohellena@gmail.com>
2020-07-15 12:17:45 -0700

    orders and lifetime value

---
+++ models/staging/stg_stripe_payments.sql
@@ -0,0 +1,9 @@
+SELECT
+  id AS stripe_payment_id,
+  orderid AS order_id,
+  paymentmethod AS payment_method,
+  status AS payment_status,
+  amount,
+  created AS created_date
+FROM
+  raw.stripe.payment
